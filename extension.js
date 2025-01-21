2a21b963972b5c11e267120974debe88139e9e42
fsx5795 <41941228+fsx5795@users.noreply.github.com>
2025-01-21 16:36:39 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-function activate(context) {
+function activate(_) {
     const vscode = require('vscode')
     const fs = require('fs')
     const path = require('path')
@@ -21,13 +21,13 @@ function activate(context) {
             fs.mkdirSync(`${workspaceFolder}/.vscode`)
         const cCppPropertiesFilePath = `${workspaceFolder}/.vscode/c_cpp_properties.json`
         if (!fs.existsSync(cCppPropertiesFilePath))
-            fs.writeFileSync(cCppPropertiesFilePath, '{\n    "configurations": [\n        {\n            "name": "qt",\n            "includePath": [\n                "' + vskitdir.replaceAll('\\', '/') + '/include/**",\n                "${workspaceRoot}/**"\n            ],\n            "cStandard": "c11",\n            "cppStandard": "c++17"\n        }\n    ],\n    "version": 4\n}')
+            fs.writeFileSync(cCppPropertiesFilePath, '{\n    "configurations": [\n        {\n            "name": "qt",\n            "includePath": [\n                "' + qtkitdir.replaceAll('\\', '/') + '/include/**",\n                "${workspaceRoot}/**"\n            ],\n            "cStandard": "c11",\n            "cppStandard": "c++17"\n        }\n    ],\n    "version": 4\n}')
         const launchFilePath = `${workspaceFolder}/.vscode/launch.json`
         if (!fs.existsSync(launchFilePath))
             fs.writeFileSync(launchFilePath, '{\n    "version": "0.2.0",\n    "configurations": [\n        {\n            "name": "Launch",\n            "type": "cppvsdbg",\n            "request": "launch",\n            "program": "${workspaceRoot}/build/debug/' + basename + '.exe",\n            "args": [],\n            "stopAtEntry": false,\n            "cwd": "${workspaceRoot}",\n            "environment": [],\n            "console": "integratedTerminal",\n            "preLaunchTask": "debug"\n        }\n    ]\n}')
         const settingsFilePath = `${workspaceFolder}/.vscode/settings.json`
         if (!fs.existsSync(settingsFilePath))
-            fs.writeFileSync(settingsFilePath, `{\n    "qtConfigure.qtKitDir": "${qtkitdir}"\n}`)
+            fs.writeFileSync(settingsFilePath, '{\n    "qtConfigure.qtKitDir": "' + qtkitdir.replaceAll('\\', '/') + '"\n}')
         const tasksFilePath = `${workspaceFolder}/.vscode/tasks.json`
         if (!fs.existsSync(tasksFilePath))
             fs.writeFileSync(tasksFilePath, '{\n    "version": "2.0.0",\n    "tasks": [\n        {\n            "label": "debug",\n            "type": "shell",\n            "command": "cmd",\n            "args": [\n                "/c",\n                "${workspaceRoot}/scripts/build_debug.bat",\n                "debug"\n            ],\n            "group": {\n                "kind": "build",\n                "isDefault": true\n            }\n        },\n        {\n            "label": "release",\n            "type": "shell",\n            "command": "cmd",\n            "args": [\n                "/c",\n                "${workspaceRoot}/scripts/build_release.bat",\n                "release"\n            ],\n            "group": {\n                "kind": "build",\n                "isDefault": true\n            }\n        }\n    ]\n}')
@@ -35,10 +35,10 @@ function activate(context) {
             fs.mkdirSync(`${workspaceFolder}/scripts`)
         const debugFilePath = `${workspaceFolder}/scripts/build_debug.bat`
         if (!fs.existsSync(debugFilePath))
-            fs.writeFileSync(debugFilePath, `@echo off\nset QT_DIR=${qtkitdir}\nset SRC_DIR=%cd%\nset BUILD_DIR=%cd%\\build\nif not exist %QT_DIR% exit\nif not exist %SRC_DIR% exit\nif not exist %BUILD_DIR% md %BUILD_DIR%\ncd build\ncall "${vskitdir}\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x64\n%QT_DIR%\\bin\\qmake.exe %SRC_DIR%\\demo.pro -spec win32-msvc  "CONFIG+=debug" "CONFIG+=console"\nif exist %BUILD_DIR%\\debug\\${basename}.exe del %BUILD_DIR%\\debug\\demo.exe\nnmake Debug\nif not exist %BUILD_DIR%\\debug\\Qt5Cored.dll (\n  %QT_DIR%\\bin\\windeployqt.exe %BUILD_DIR%\\debug\\demo.exe)`)
+            fs.writeFileSync(debugFilePath, '@echo off\nset QT_DIR=' + qtkitdir.replaceAll('/', '\\') + '\nset SRC_DIR=%cd%\nset BUILD_DIR=%cd%\\build\nif not exist %QT_DIR% exit\nif not exist %SRC_DIR% exit\nif not exist %BUILD_DIR% md %BUILD_DIR%\ncd build\ncall "' + vskitdir + '\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x64\n%QT_DIR%\\bin\\qmake.exe %SRC_DIR%\\demo.pro -spec win32-msvc  "CONFIG+=debug" "CONFIG+=console"\nif exist %BUILD_DIR%\\debug\\' + basename + '.exe del %BUILD_DIR%\\debug\\demo.exe\nnmake Debug\nif not exist %BUILD_DIR%\\debug\\Qt5Cored.dll (\n  %QT_DIR%\\bin\\windeployqt.exe %BUILD_DIR%\\debug\\demo.exe)')
         const releaseFilePath = `${workspaceFolder}/scripts/build_release.bat`
         if (!fs.existsSync(releaseFilePath))
-            fs.writeFileSync(releaseFilePath, `@echo off\nset QT_DIR=${qtkitdir}\nset SRC_DIR=%cd%\nset BUILD_DIR=%cd%\\build\nif not exist %QT_DIR% exit\nif not exist %SRC_DIR% exit\nif not exist %BUILD_DIR% md %BUILD_DIR%\ncd build\ncall "${vskitdir}\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x64\n%QT_DIR%\bin\qmake.exe %SRC_DIR%\demo.pro -spec win32-msvc  "CONFIG+=release"\nif exist %BUILD_DIR%\\release\\demo.exe del %BUILD_DIR%\\release\\${basename}.exe\nnmake Release\nif not exist %BUILD_DIR%\\release\\Qt5Core.dll (\n  %QT_DIR%\\bin\\windeployqt.exe %BUILD_DIR%\\release\\demo.exe\n)`)
+            fs.writeFileSync(releaseFilePath, '@echo off\nset QT_DIR=' + qtkitdir.replaceAll('/', '\\') + '\nset SRC_DIR=%cd%\nset BUILD_DIR=%cd%\\build\nif not exist %QT_DIR% exit\nif not exist %SRC_DIR% exit\nif not exist %BUILD_DIR% md %BUILD_DIR%\ncd build\ncall "' + vskitdir + '\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x64\n%QT_DIR%\bin\qmake.exe %SRC_DIR%\demo.pro -spec win32-msvc  "CONFIG+=release"\nif exist %BUILD_DIR%\\release\\demo.exe del %BUILD_DIR%\\release\\' + basename + '.exe\nnmake Release\nif not exist %BUILD_DIR%\\release\\Qt5Core.dll (\n  %QT_DIR%\\bin\\windeployqt.exe %BUILD_DIR%\\release\\demo.exe\n)')
     }
     /*
     context.subscriptions.push(
